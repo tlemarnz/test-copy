3c74dd846f12f184dc2ee6c9fd959bb182f6e5fc
jedsada <petchkeaw23@gmail.com>
2018-01-29 16:42:05 +0700

    gg

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <html>
   <head>
-<script src="https://cdn.rawgit.com/zenorocha/clipboard.js/v1.7.1/dist/clipboard.min.js"></script>
-
-
   </head>
   <body>
-   <!-- 1. Define some markup -->
-    <button class="btn" data-clipboard-text="1">Copy</button>
-    <button class="btn" data-clipboard-text="2">Copy</button>
-    <button class="btn" data-clipboard-text="3">Copy</button>
+ <button class="btn">Copy</button>
+    <div>hello</div>
 
     <!-- 2. Include library -->
-    <script src="../dist/clipboard.min.js"></script>
+    <script src="clipboard.min.js"></script>
+  </body>
+<script>
+    var clipboard = new Clipboard('.btn', {
+        target: function() {
+            return document.querySelector('div');
+        }
+    });
 
-    <!-- 3. Instantiate clipboard by passing a string selector -->
-    <script>
-    var clipboard = new Clipboard('.btn');
     clipboard.on('success', function(e) {
         console.log(e);
     });
+
     clipboard.on('error', function(e) {
         console.log(e);
     });
     </script>
-  </body>
 </html>
