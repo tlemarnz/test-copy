4ec78d6890dd5ac38e0fb197dc6a256b29b089d4
jedsada <petchkeaw23@gmail.com>
2018-02-12 14:38:25 +0700

    nnnn

--- index.php
+++ index.php
@@ -2,25 +2,39 @@
   <head>
   </head>
   <body>
- <button class="btn">Copy</button>
-    <div>tle</div>
-
-    <!-- 2. Include library -->
-    <script src="clipboard.min.js"></script>
+		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>
+				<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>		<div>Try scrolling the iframe.</div>
+		<p>Paragraph - <span>Scroll happened!</span></p>
+		<input type="text" class="test-input">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
   </body>
 <script>
-    var clipboard = new Clipboard('.btn', {
-        target: function() {
-            return document.querySelector('div');
-        }
-    });
-
-    clipboard.on('success', function(e) {
-        console.log(e);
-    });
-
-    clipboard.on('error', function(e) {
-        console.log(e);
-    });
-    </script>
+		$(window).scroll(function(e) {
+			var timeout = null;
+			clearTimeout(timeout);
+			timeout = setTimeout(function() {
+				console.log('xxxxx');
+				var scroll = $(window).scrollTop();
+				$('.test-input').val(scroll);
+				
+			}, 200);
+		});
+</script>
 </html>
