acddbe0b11052cf39f3a3e816858678b73323926
jedsada <petchkeaw23@gmail.com>
2018-02-22 09:56:32 +0700

    kk

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
+  <script type="text/javascript">
 $(document).ready(function () {
 	$( "a" ).click(function() {
  var str = "cs-";
